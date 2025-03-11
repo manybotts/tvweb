8ec4c6625040dd92c6b1656ed745d7f42e5f39da
manybotts <105394368+manybotts@users.noreply.github.com>
2025-03-11 13:25:21 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,10 +1,15 @@
 DROP TABLE IF EXISTS tv_shows;
+
 CREATE TABLE tv_shows (
-  message_id INTEGER PRIMARY KEY,
-  show_name TEXT NOT NULL,
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  show_name TEXT UNIQUE NOT NULL,
   season_episode TEXT,
   download_link TEXT,
-  poster_path TEXT,
+  message_id INTEGER,
   overview TEXT,
-  vote_average REAL
+  vote_average REAL,
+  poster_path TEXT
 );
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_show_name ON tv_shows(show_name);
+CREATE INDEX IF NOT EXISTS idx_message_id ON tv_shows(message_id);
