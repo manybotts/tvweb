120453eca164d25c8aadcee435e662b43622638c
manybotts <105394368+manybotts@users.noreply.github.com>
2025-03-12 07:01:17 +0300

    Update models.py

--- tv_app/models.py
+++ tv_app/models.py
@@ -1,15 +1,21 @@
+# models.py
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
 
-db = SQLAlchemy()
+db = SQLAlchemy()  # Create the db object here
 
 class TVShow(db.Model):
+    __tablename__ = 'tv_shows'
+
     id = db.Column(db.Integer, primary_key=True)
     message_id = db.Column(db.Integer, unique=True, nullable=False)
     show_name = db.Column(db.String, nullable=False)
-    episode_details = db.Column(db.String, nullable=False)
+    season_episode = db.Column(db.String)  # Allow None
     download_link = db.Column(db.String)
-    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    overview = db.Column(db.Text)
+    vote_average = db.Column(db.Float)
+    poster_path = db.Column(db.String)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)  # Use utcnow
 
     def __repr__(self):
-        return f'<TVShow {self.show_name} - {self.episode_details}>'
+        return f'<TVShow {self.show_name}>'
