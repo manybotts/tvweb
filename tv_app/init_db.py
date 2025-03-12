8881752344b56970df9fa5cd8025103ccd17ce1b
manybotts <105394368+manybotts@users.noreply.github.com>
2025-03-12 11:29:18 +0300

    Update init_db.py

--- tv_app/init_db.py
+++ tv_app/init_db.py
@@ -1,6 +1,6 @@
 # tv_app/init_db.py
-from models import db, TVShow  # Import your models
-from app import app  # Import your Flask app
+from .models import db, TVShow  # Use relative import
+from .app import app  # Use relative import
 
 with app.app_context():
     db.create_all()
