59fdc09df2f0ae53743053bf45f307e4a2f3c95b
manybotts <105394368+manybotts@users.noreply.github.com>
2025-03-12 06:02:02 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,17 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
 
-db = SQLAlchemy()  # Create the db object here
+db = SQLAlchemy()
 
 class TVShow(db.Model):
-    __tablename__ = 'tv_shows'
-
     id = db.Column(db.Integer, primary_key=True)
     message_id = db.Column(db.Integer, unique=True, nullable=False)
     show_name = db.Column(db.String, nullable=False)
-    episode_title = db.Column(db.String, nullable=False)
-    download_link = db.Column(db.String, nullable=False)
+    episode_details = db.Column(db.String, nullable=False)
+    download_link = db.Column(db.String)
     created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
 
     def __repr__(self):
-        return f'<TVShow {self.show_name} - {self.episode_title}>'
+        return f'<TVShow {self.show_name} - {self.episode_details}>'
