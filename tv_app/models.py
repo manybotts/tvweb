7cb3280f0e271a6a2f052cc905c7bf7a0b1538c9
manybotts <105394368+manybotts@users.noreply.github.com>
2025-03-12 10:52:53 +0300

    Update models.py

--- tv_app/models.py
+++ tv_app/models.py
@@ -10,12 +10,13 @@ class TVShow(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     message_id = db.Column(db.Integer, unique=True, nullable=False)
     show_name = db.Column(db.String, nullable=False)
-    episode_title = db.Column(db.String)  # USE THIS FIELD
+    episode_title = db.Column(db.String)
     download_link = db.Column(db.String)
     overview = db.Column(db.Text)
     vote_average = db.Column(db.Float)
     poster_path = db.Column(db.String)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    clicks = db.Column(db.Integer, default=0)  # ADD THIS LINE
 
     def __repr__(self):
-        return f'<TVShow {self.show_name} - {self.episode_title}>' #USE episode_title here
+        return f'<TVShow {self.show_name} - {self.episode_title}>'
