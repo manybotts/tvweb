7fdce44bb4b18d52a309bae3b0f0d491574564be
manybotts <105394368+manybotts@users.noreply.github.com>
2025-03-13 13:19:07 +0300

    Update init_db.py

--- tv_app/init_db.py
+++ tv_app/init_db.py
@@ -1,7 +1,12 @@
 # tv_app/init_db.py
-from .models import db, TVShow  # Use relative import
-from .app import app  # Use relative import
+from .models import db, TVShow
+from .app import app
+from sqlalchemy import inspect
 
 with app.app_context():
-    db.create_all()
-    print("Database tables created successfully!")
+    inspector = inspect(db.engine)
+    if not inspector.has_table(TVShow.__tablename__):  # Check if table exists
+        db.create_all()
+        print("Database tables created successfully!")
+    else:
+        print("Database tables already exist.")
