446a36b3c3460e9cc62a8be24aa7be6f6ad27047
manybotts <105394368+manybotts@users.noreply.github.com>
2025-03-13 15:39:08 +0300

    Update init_db.py

--- tv_app/init_db.py
+++ tv_app/init_db.py
@@ -1,13 +1,13 @@
-# tv_app/init_db.py
-from .app import create_app
+#init_db.py
+from .app import create_app #Import create_app function
 from .models import db, TVShow
-from sqlalchemy import inspect  # Import the inspect function
+from sqlalchemy import inspect
 
-app = create_app()
+app = create_app() # Call create_app to get the app instance
 
 with app.app_context():
     inspector = inspect(db.engine)
-    if not inspector.has_table(TVShow.__tablename__):  # Check if table exists
+    if not inspector.has_table(TVShow.__tablename__):
         db.create_all()
         print("Database tables created successfully!")
     else:
