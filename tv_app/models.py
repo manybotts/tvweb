e7192b20cf289c25f322cfe8c32e119046abd7e3
manybotts <105394368+manybotts@users.noreply.github.com>
2025-03-14 07:03:52 +0300

    Update models.py

--- tv_app/models.py
+++ tv_app/models.py
@@ -1,22 +1,18 @@
-# models.py
+# tv_app/models.py
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, timezone
+from datetime import datetime
 
-db = SQLAlchemy()  # Create the db object here
+db = SQLAlchemy()
 
 class TVShow(db.Model):
     __tablename__ = 'tv_shows'
 
     id = db.Column(db.Integer, primary_key=True)
-    message_id = db.Column(db.Integer, unique=True, nullable=False)
-    show_name = db.Column(db.String, nullable=False)
-    episode_title = db.Column(db.String)
-    download_link = db.Column(db.String)
+    message_id = db.Column(db.String, nullable=False, index=True)
+    show_name = db.Column(db.String, nullable=False, index=True)
+    episode_title = db.Column(db.String, default=None)
+    download_link = db.Column(db.String, default=None)
     overview = db.Column(db.Text)
     vote_average = db.Column(db.Float)
-    poster_path = db.Column(db.String)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    clicks = db.Column(db.Integer, default=0)  # ADD THIS LINE
-
-    def __repr__(self):
-        return f'<TVShow {self.show_name} - {self.episode_title}>'
+    poster_path = db.Column(db.String, default=None)
+    created_at = db.Column(db.DateTime,
