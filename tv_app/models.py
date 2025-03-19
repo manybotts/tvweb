80cc056c0e9a69d2dddbfda2ec1cdc419fb71503
manybotts <105394368+manybotts@users.noreply.github.com>
2025-03-19 08:26:12 +0300

    Update models.py

--- tv_app/models.py
+++ tv_app/models.py
@@ -15,9 +15,12 @@ class TVShow(db.Model):
     overview = db.Column(db.Text)
     vote_average = db.Column(db.Float)
     poster_path = db.Column(db.String, default=None)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow) # Line 18 - Check this!
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
     clicks = db.Column(db.Integer, default=0)
     content_hash = db.Column(db.String(64), nullable=False, unique=True, index=True)
+    genre = db.Column(db.Text)  # Added genre
+    year = db.Column(db.Integer)  # Added year
+    season_range = db.Column(db.Text)  # Added season_range
 
     def __repr__(self):
         return f'<TVShow {self.show_name} - {self.episode_title}>'
