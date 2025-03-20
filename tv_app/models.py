455cebe53954da0b6815cba7068d7e433e623514
manybotts <105394368+manybotts@users.noreply.github.com>
2025-03-20 16:15:05 +0300

    Update models.py

--- tv_app/models.py
+++ tv_app/models.py
@@ -8,16 +8,20 @@ class TVShow(db.Model):
     __tablename__ = 'tv_shows'
 
     id = db.Column(db.Integer, primary_key=True)
-    message_id = db.Column(db.String, nullable=False, index=True)
+    message_id = db.Column(db.Integer, unique=True, nullable=False, index=True)
     show_name = db.Column(db.String, nullable=False, index=True)
     episode_title = db.Column(db.String, default=None)
     download_link = db.Column(db.String, default=None)
     overview = db.Column(db.Text)
     vote_average = db.Column(db.Float)
     poster_path = db.Column(db.String, default=None)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow) # Line 18 - Check this!
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, index=True)  # Added index=True
     clicks = db.Column(db.Integer, default=0)
-    content_hash = db.Column(db.String(64), nullable=False, unique=True, index=True)
+    content_hash = db.Column(db.String(64), nullable=False, index=True)
+
+    __table_args__ = (
+        db.Index('ix_show_name_episode_title', 'show_name', 'episode_title'),  # Combined index
+    )
 
     def __repr__(self):
         return f'<TVShow {self.show_name} - {self.episode_title}>'
