0d32e28b885e05d071a54fccf98644c37af2c3d0
manybotts <105394368+manybotts@users.noreply.github.com>
2025-03-20 14:10:02 +0300

    Delete tv_app directory

--- tv_app/run_tasks.py
+++
@@ -1,7 +0,0 @@
-# tv_app/run_task.py
-from tv_app.tasks import update_tv_shows
-from tv_app.app import app  # Import the 'app' instance directly
-
-if __name__ == "__main__":
-    with app.app_context():
-        update_tv_shows()
