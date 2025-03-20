f7dbeda3aee37fdb6edbadb26a07fad11703d94f
manybotts <105394368+manybotts@users.noreply.github.com>
2025-03-20 08:36:51 +0300

    Update __init__.py

--- tv_app/__init__.py
+++ tv_app/__init__.py
@@ -1,2 +1,13 @@
 
 # tv_app/init_db.py
+# tv_app/__init__.py
+# This file can remain relatively simple.  We're *not* using the
+# application factory pattern here.
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+db = SQLAlchemy()
