9f297a062461166d23cca839a7908ff0344580ae
manybotts <105394368+manybotts@users.noreply.github.com>
2025-03-20 08:01:27 +0300

    Update models.py

--- tv_app/models.py
+++ tv_app/models.py
@@ -9,7 +9,7 @@ db = SQLAlchemy()
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(256))  # Increased to 256
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
