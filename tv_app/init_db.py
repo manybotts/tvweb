f3d8dc622f835973da1db746e9a91da588423161
manybotts <105394368+manybotts@users.noreply.github.com>
2025-03-20 17:24:48 +0300

    Update init_db.py

--- tv_app/init_db.py
+++ tv_app/init_db.py
@@ -6,11 +6,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def create_tables():
-    """Creates the tv_shows table in the database if it doesn't exist."""
+    """Creates the database tables if they don't exist."""
 
     conn = None
     try:
-        # Use DATABASE_URL directly!
         conn = psycopg2.connect(os.environ.get("DATABASE_URL"))
         cur = conn.cursor()
 
@@ -20,6 +19,14 @@ def create_tables():
         cur.execute("CREATE EXTENSION IF NOT EXISTS pg_trgm;")
 
         if not table_exists:
+            # Create genres table
+            cur.execute("""
+                CREATE TABLE genres (
+                    id SERIAL PRIMARY KEY,
+                    name VARCHAR(50) UNIQUE NOT NULL
+                );
+            """)
+            # Create tv_shows table
             cur.execute("""
                 CREATE TABLE tv_shows (
                     id SERIAL PRIMARY KEY,
@@ -32,9 +39,24 @@ def create_tables():
                     poster_path VARCHAR,
                     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
                     clicks INTEGER DEFAULT 0,
-                    content_hash VARCHAR(64) NOT NULL
+                    content_hash VARCHAR(64) NOT NULL,
+                    year INTEGER,
+                    rating FLOAT
                 );
             """)
+
+            # Create show_genres association table
+            cur.execute("""
+                CREATE TABLE show_genres (
+                    tvshow_id INTEGER NOT NULL,
+                    genre_id INTEGER NOT NULL,
+                    PRIMARY KEY (tvshow_id, genre_id),
+                    FOREIGN KEY (tvshow_id) REFERENCES tv_shows(id) ON DELETE CASCADE,
+                    FOREIGN KEY (genre_id) REFERENCES genres(id) ON DELETE CASCADE
+                );
+            """)
+
+            # Create indexes
             cur.execute("CREATE INDEX ix_tv_shows_message_id ON tv_shows (message_id);")
             cur.execute("CREATE INDEX ix_tv_shows_show_name ON tv_shows (show_name);")
             cur.execute("CREATE INDEX ix_tv_shows_created_at ON tv_shows (created_at);")
@@ -42,11 +64,12 @@ def create_tables():
             cur.execute("CREATE INDEX ix_show_name_episode_title ON tv_shows (show_name, episode_title);")
             cur.execute("CREATE INDEX ix_show_name_trgm ON tv_shows USING gin (show_name gin_trgm_ops);")
 
+
             conn.commit()
-            print("Table 'tv_shows' created successfully (if it didn't exist)!")
+            print("Tables created successfully!")
 
     except (Exception, psycopg2.DatabaseError) as error:
-        print(f"Error creating table: {error}")
+        print(f"Error creating tables: {error}")
         if conn:
             conn.rollback()
     finally:
